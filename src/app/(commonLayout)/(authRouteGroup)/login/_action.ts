960203f60959c2e05fd48347b8528dd1e44592a8
Md Soriful hasan <sorifullhasan300@gmail.com>
2026-03-17 21:46:23 +0600

    feat(auth): enhance login action with role-based redirect and password change logic

--- src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
+++ src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
@@ -4,12 +4,19 @@
 import { httpClient } from "@/lib/axios/httpClient";
 import { ApiErrorResponse } from "@/types/api.type";
 import { ILoginResponse } from "@/types/auth.type";
+import {
+  getDefaultDashboardRoute,
+  isValidRedirectForRole,
+  UserRole,
+} from "@/utils/authUtils";
 import { setTokenInCookies } from "@/utils/tokenUtils";
+
 import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
 import { redirect } from "next/navigation";
 
 export const loginAction = async (
   payload: ILoginPayload,
+  redirectPath?: string,
 ): Promise<ILoginResponse | ApiErrorResponse> => {
   const parsedPayload = loginZodSchema.safeParse(payload);
 
@@ -25,15 +32,31 @@ export const loginAction = async (
       "/auth/login",
       parsedPayload.data,
     );
-    console.log(response.data);
 
-    const { accessToken, refreshToken, token } = response.data;
+    const { accessToken, refreshToken, token, user } = response.data;
+    const { role, emailVerified, needPasswordChange, email } = user;
     await setTokenInCookies("accessToken", accessToken);
     await setTokenInCookies("refreshToken", refreshToken);
     await setTokenInCookies("better-auth.session_token", token, 24 * 60 * 60); // 1 day in seconds
 
-    redirect("/dashboard");
+    // if(!emailVerified){
+    //     redirect("/verify-email");
+    // }else // in the catch block
+
+    if (needPasswordChange) {
+      //TODO : refactoring
+      redirect(`/reset-password?email=${email}`);
+    } else {
+      // redirect(redirectPath || "/dashboard");
+      const targetPath =
+        redirectPath && isValidRedirectForRole(redirectPath, role as UserRole)
+          ? redirectPath
+          : getDefaultDashboardRoute(role as UserRole);
+
+      redirect(targetPath);
+    }
   } catch (error: any) {
+    console.log(error, "error");
     if (
       error &&
       typeof error === "object" &&
@@ -42,6 +65,14 @@ export const loginAction = async (
       error.digest.startsWith("NEXT_REDIRECT")
     ) {
       throw error;
+    }
+
+    if (
+      error &&
+      error.response &&
+      error.response.data.message === "Email not verified"
+    ) {
+      redirect(`/verify-email?email=${payload.email}`);
     }
     return {
       success: false,
--- src/lib/axios/httpClient.ts
+++ src/lib/axios/httpClient.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getNewTokensWithRefreshToken } from "@/service/auth.service";
 import { ApiResponse } from "@/types/api.type";
+import { isTokenExpiringSoon } from "@/utils/tokenUtils";
 import axios from "axios";
+import { cookies, headers } from "next/headers";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -7,12 +11,41 @@ if (!API_BASE_URL) {
   throw new Error("API_BASE_URL is not defined in environment variables");
 }
 
-const axiosInstance = () => {
+async function tryRefreshToken(accessToken: string, refreshToken: string) {
+  if (!isTokenExpiringSoon(accessToken)) {
+    return;
+  }
+  const requestHeader = await headers();
+  if (requestHeader.get("x-token-refreshed") === "1") {
+    return; // avoid multiple refresh attempts in the same request lifecycle
+  }
+
+  try {
+    await getNewTokensWithRefreshToken(refreshToken);
+  } catch (error: any) {
+    console.error("Error refreshing token in http client:", error);
+  }
+}
+
+const axiosInstance = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+  if (accessToken && refreshToken) {
+    await tryRefreshToken(accessToken, refreshToken);
+  }
+  // eg Cookie: "accessToken=abc123; refreshToken=def456"
+  const cookieHeader = cookieStore
+    .getAll()
+    .map((cookie) => `${cookie.name}=${cookie.value}`)
+    .join("; ");
+
   const instance = axios.create({
     baseURL: API_BASE_URL,
     timeout: 30000,
     headers: {
       "Content-Type": "application/json",
+      Cookie: cookieHeader,
     },
   });
 
@@ -29,7 +62,7 @@ const httpGet = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const instance = axiosInstance();
+    const instance = await axiosInstance();
     const response = await instance.get<ApiResponse<TData>>(endpoint, {
       params: options?.params,
       headers: options?.headers,
@@ -47,14 +80,12 @@ const httpPost = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().post<ApiResponse<TData>>(
-      endpoint,
-      data,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.post<ApiResponse<TData>>(endpoint, data, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error(`POST request to ${endpoint} failed:`, error);
@@ -68,14 +99,12 @@ const httpPut = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().put<ApiResponse<TData>>(
-      endpoint,
-      data,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.put<ApiResponse<TData>>(endpoint, data, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error(`PUT request to ${endpoint} failed:`, error);
@@ -89,14 +118,12 @@ const httpPatch = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().patch<ApiResponse<TData>>(
-      endpoint,
-      data,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.patch<ApiResponse<TData>>(endpoint, data, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error(`PATCH request to ${endpoint} failed:`, error);
@@ -109,13 +136,12 @@ const httpDelete = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().delete<ApiResponse<TData>>(
-      endpoint,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.delete<ApiResponse<TData>>(endpoint, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error(`DELETE request to ${endpoint} failed:`, error);
